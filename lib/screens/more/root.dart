4cc1fe5782fd55d000c0a0042fbe9e15c5895fd2
lewiseman <lewisemann@gmail.com>
2021-06-15 13:36:52 +0300

    Rosary App 1

--- lib/screens/more/root.dart
+++ lib/screens/more/root.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:share_plus/share_plus.dart';
 
 class More extends StatelessWidget {
   const More({Key? key}) : super(key: key);
@@ -31,36 +32,30 @@ class More extends StatelessWidget {
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: ListTile(
             title: Text(
-              'Share App',
+              'Change language',
               style: GoogleFonts.poppins(),
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.arrow_forward_ios_rounded),
-              onPressed: () {
-                //Share.share('check out my website https://example.com');
-              },
-            ),
+            trailing: Icon(Icons.language_rounded),
             tileColor: CupertinoColors.white,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            onTap: () {},
+            onTap: () {
+            },
           ),
         ),
         Padding(
           padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
           child: ListTile(
             title: Text(
-              'Change language',
+              'Share App',
               style: GoogleFonts.poppins(),
             ),
-            trailing: IconButton(
-              icon: Icon(Icons.arrow_forward_ios_rounded),
-              onPressed: () {},
-            ),
+            trailing: Icon(Icons.offline_share_rounded),
             tileColor: CupertinoColors.white,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            onTap: () {},
+            onTap: () =>
+                Share.share('check out my website https://example.com'),
           ),
         ),
         Padding(
@@ -74,7 +69,7 @@ class More extends StatelessWidget {
             tileColor: CupertinoColors.white,
             shape:
                 RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-            onTap: ()=> SystemNavigator.pop(),
+            onTap: () => SystemNavigator.pop(),
           ),
         ),
       ],
